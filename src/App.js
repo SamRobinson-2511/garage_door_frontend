d4468e7ca3ba35ece659301f9cb77056c9d9bf2c
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-23 16:19:14 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Switch } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+// import './App.css';
+import FormData from './LoginForm'  
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
